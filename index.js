8583b56d44871d5c27c76971055d17361414bd4d
Johanna Hopp <johannahopp96@web.de>
2022-10-18 11:51:03 +0200

    add fetch data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {createCharacterCard} from './components/card/card.js';
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]',
@@ -7,8 +9,28 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+const apiUrl = 'https://rickandmortyapi.com/api/character';
 
 // States
 let maxPage = 1;
 let page = 1;
 let searchQuery = '';
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(response.status + ': fetching data failed!');
+    }
+    const characters = await response.json();
+    // console.log(characters);
+    cardContainer.innerHTML = '';
+    characters.results.forEach(character => {
+      const characterCard = createCharacterCard(character);
+      cardContainer.append(characterCard);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchCharacters();
