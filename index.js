e5101b61071210fa786b8a16ae784986106bab19
Christian Koch <christian.koch@mail.schwarz>
2022-10-18 12:21:19 +0200

    add paging functionality

--- index.js
+++ index.js
@@ -16,21 +16,40 @@ let maxPage = 1;
 let page = 1;
 let searchQuery = '';
 
-async function fetchCharacters() {
+async function fetchCharacters(currentPage) {
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl + `?page=${currentPage}`);
     if (!response.ok) {
       throw new Error(response.status + ': fetching data failed!');
     }
     const characters = await response.json();
+    console.log();
+    maxPage = characters.info.pages;
     // console.log(characters);
     cardContainer.innerHTML = '';
     characters.results.forEach(character => {
       const characterCard = createCharacterCard(character);
       cardContainer.append(characterCard);
     });
+    pagination.textContent = `${page} / ${maxPage}`;
   } catch (error) {
     console.error(error);
   }
 }
-fetchCharacters();
+
+nextButton.addEventListener('click', () => {
+  console.log('next button clicked ' + page + ' ' + maxPage);
+  if (page < maxPage) {
+    page++;
+    fetchCharacters(page);
+  }
+});
+
+prevButton.addEventListener('click', () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters(page);
+  }
+});
+
+fetchCharacters(page);
